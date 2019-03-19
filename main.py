e4d5e45034cc19085f5bb7e2d9b450c1677f008f
Robin Verleun <verleun.ro@gmail.com>
2019-03-19 02:36:15 -0600

    Finished functionality

--- main.py
+++ main.py
@@ -11,38 +11,38 @@ import json
 import os
 import datetime
 import re
-
+from collections import defaultdict
 
 
 def get_cli_input():
 		parser = argparse.ArgumentParser(description=__doc__)
 		parser.add_argument('input', help='path to the input file', metavar='INPUT_FILE')
+		parser.add_argument('--v', '--verbose', dest='verbose', help='output line errors', action='store_true')
 		args = parser.parse_args()
 
 		return args
 
 
-
+'''
+	Validate the fields of the file sequentially.
+	Store the return from the name validation to re-use the name and extension
+	for creating the dictionary entry at the end of the function.
+'''
 def validate_file_log(file, index):
-	# print('{0}    :{1}'.format(index, file))
-
-	# Valid flag for tracking the progress of the file validation
-	valid = True;
-	
 	validate_timestamp(file['ts'])
 	validate_time(file['pt'])
+	validate_sha(file['sha'])
+	validate_path(file['ph'], file['nm'])
+	validate_disposition(file['dp'])
 
 	all_pattern = createRegexPattern('[0-5]')
 	validate_UUID(file['si'], all_pattern)
 	validate_UUID(file['uu'], all_pattern)
 	validate_UUID(file['bg'], all_pattern)
-
-	validate_sha(file['sha'])
-
-	name, ext = validate_file_name(file['nm'])
-	validate_path(file['ph'], name, ext)
 	
-	# validate_disposition(file['dp'])
+	name, ext = validate_file_name(file['nm'])
+
+	return (name, ext)
 
 
 
@@ -98,9 +98,7 @@ def validate_UUID(uuid_to_test, pattern):
 	if(len(uuid_to_test) != 36): 
 		raise ValueError('Invalid UUID length')
 
-	if(pattern.match(uuid_to_test)):
-		return True
-	else:
+	if(not pattern.match(uuid_to_test)):
 		raise ValueError('Value {0}: Invalid UUID format'.format(uuid_to_test))
 
 
@@ -120,7 +118,10 @@ def validate_sha(sha):
 		raise ValueError('Value {0}: Invalid sha16 value'.format(sha))
 
 
-
+'''
+	Take in a filename and check to see if it starts or ends with slashes,
+	then confirm it has an extension using the OS libraries.
+'''
 def validate_file_name(name):
 	# Check that the filename doesn't start or end with a slash
 	if(name.startswith(('/', '\\')) or name.endswith(('/', '\\'))):
@@ -132,16 +133,35 @@ def validate_file_name(name):
 
 
 
-def validate_path(path, name, ext):
-	# Assumption: filepaths don't need leading or trailing slashes to be valid
+'''
+	Take in a filepath and confirm that it matches the given filename in the log.
+	Assumption: filepaths don't need leading or trailing slashes to be valid
+	Assumption: filepath only needs the name of the file at the end to be valid
+'''
+def validate_path(path, name):
 	_path, _file = os.path.split(path)
-	return
+	if(_file != name):
+		raise ValueError('Path \'{0}\' does not match the given filename \'{1}\''.format(path, name))
 
 
 
+
+'''
+	Check the disposition of the log. If it is not a value of 1, 2, or 3, it is considered invalid.
+'''
 def validate_disposition(dp):
-	return
+	try:
+		disposition = int(dp)
+		if(dp != 1 and dp != 2 and dp != 3):
+			raise Exception
+	except:
+		raise ValueError('Value {0}: Dispositon is invalid'.format(dp))
 
+
+
+def print_result(extensions):
+	for key, value in extensions.items():
+		print('{0}: {1}'.format(key, len(value)))
 
 
 def main():
@@ -150,45 +170,31 @@ def main():
 	# try:except block to catch file open errors.
 	try:
 		with open(args.input, 'r') as file:
-			for num, line in enumerate(file, start=1):
 
+			extension_set = defaultdict(set);
+			for num, line in enumerate(file, start=1):
+			
 				# loop over the values, attempting to validate. Catch ValueErrors thrown by the validator. 
 				try:
 					json_file = json.loads(line)
-					validate_file_log(json.loads(line), num)
+					name, ext = validate_file_log(json.loads(line), num)
+					
+					# If we're done validating without ValueError's, add to dict.
+					extension_set[ext].add(name)
+				
 				except ValueError as value_error:
-					print('Invalid line (#{0}): {1}'.format(num, value_error))
+					if(args.verbose):
+						print('Invalid line (#{0}): {1}'.format(num, value_error))
 				except Exception as e:
 					print('Issue loading line {0}: {1}'.format(num, e))
-					
+
+			print_result(extension_set)	
+
 	except Exception as e:
-		print('Error opening file.')
+		print('Error', e)
 
 
 
 # Execute main function
 if __name__ == "__main__":
 	main()
-
-
-# TODO: cast into a list
-
-# TODO: For Loop
-# TODO: Cast to json and cath error
-# TODO: Validate timestamps
-# TODO: validate UUID4'
-# TODO: validate the sha256
-# TODO: validate the disposition
-# TODO: validate the name has a filename.extension format
-# TODO: validate the path is a correct relative path
-# TODO: Create a dict of sets. Key is the extension, value is the set of names
-# TODO: End loop
-# TODO: count the set sizes and return the key:size dict
-
-
-#my_list = [json.loads(line.rstrip('\n')) for line in args.input]
-
-#print(my_list[0].keys())
-# for line in args.input:
-
-#     print(line.strip(), file=args.output)
